35adfc27c31f73f02a2547b8dd362f1b874baaab
JaydanZombies <jaydanZombies@zombies.com>
2025-07-28 11:22:47 -0500

    Mi primera rama

---
+++ src/01-edicion/03-comentarios.ts
@@ -0,0 +1,27 @@
+//*
+//*    Objetivo:
+//*        Comentar un bloque de código
+//*
+//*    Tips:
+//*        ⌘ /
+//*        
+//*        Ctrl + \
+//*
+
+// const a = 10;
+// const b = 20;
+// const c = { a, b }; 
+// *? Para comentar todo un bloque de codigo lo hacemos con "ctrl + /"
+
+
+const a = 30;
+const b = 40;
+const c = { a, b };
+
+
+console.log( a, b, c );
+
+// Resultado final
+// Sólo dejar un bloque comentado
+
+
---
+++ src/01-edicion/07-borrar-lineas.ts
@@ -0,0 +1,33 @@
+//*
+//*    Objetivo:
+//*        Comentar un bloque de código
+//*
+//*    Tips:
+//*        ⇧ ⌘ K
+//*        Ctrl + Shift + K
+//*
+//*    PRO:
+//*        Seleccionar todas las ocurrencias de la selección
+//*        ⇧ ⌘ L
+//*        Ctrl + Shift + L
+//*
+
+let NoMeBorres = ':)';
+// let Borrame = ':(';
+
+NoMeBorres = '1';
+// Borrame    = 'a';
+NoMeBorres = '1'; 
+// Borrame    = 'a';
+
+//**
+// *! Comentario de alerta - para eliminar una linea directamente 'Ctrl + shift + K'
+// *? Para seleccionar una misma palabra varias veces con 'Ctrl + D'
+//  */
+
+// Resultado final (sin los comentarios claro.)
+
+// let NoMeBorres = ':)';
+
+// NoMeBorres = '1';
+// NoMeBorres = '1';
